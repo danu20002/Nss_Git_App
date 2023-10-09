1e0ab7112abbc746620555b201c0d647c3f0b21f
Danesh Naik <naikdanesh2@gmail.com>
2023-10-10 00:04:30 +0530

    Updated now

--- app/src/main/java/com/daneshnaik/nssgit/updates.java
+++ app/src/main/java/com/daneshnaik/nssgit/updates.java
@@ -2,20 +2,127 @@ package com.daneshnaik.nssgit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.daneshnaik.Tables.updates_table;
+import com.daneshnaik.updates_Adapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class updates extends AppCompatActivity {
     BottomNavigationView bottom_nav_updates;
+    FloatingActionButton floatingActionButton_updates;
+
+    androidx.appcompat.widget.SearchView searchView_updates;
+   updates_Adapter adapter;
+   RecyclerView recyclerview_updates;
+
+    int id =0;
+    ArrayList<updates_table> UpdatesArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_updates);
+
+        floatingActionButton_updates=findViewById(R.id.floating_btn_update);
+
+        if(FirebaseAuth.getInstance().getCurrentUser().getEmail() != "nssgitofficial@gmail.com"){
+              floatingActionButton_updates.setVisibility(View.INVISIBLE);
+        }else{
+            floatingActionButton_updates.setVisibility(View.VISIBLE);
+        }
+
+
+       floatingActionButton_updates.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(new Intent(getApplicationContext(), updates_taker.class));
+
+           }
+       });
+
+
+        final String senderId = FirebaseAuth.getInstance().getUid();
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        UpdatesArrayList = new ArrayList<>();
+        adapter = new updates_Adapter(this, UpdatesArrayList);
+
+        recyclerview_updates = findViewById(R.id.recyclerview_updates);
+
+        recyclerview_updates.setAdapter(adapter);
+        recyclerview_updates.smoothScrollToPosition(0);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerview_updates.setLayoutManager(layoutManager);
+
+
+        database.getReference().child("Updates").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UpdatesArrayList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    updates_table tables = dataSnapshot.getValue(updates_table.class);
+                    UpdatesArrayList.add(tables);
+
+                    if(snapshot.exists()){
+                        id=(int) snapshot.getChildrenCount();
+                    }
+
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        searchView_updates=findViewById(R.id.searchview_updates);
+        searchView_updates.clearFocus();
+
+    searchView_updates.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            filterList(newText);
+            return true;
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -54,5 +161,21 @@ public class updates extends AppCompatActivity {
                 return true;
             }
         });
+    }
+    private  void filterList(String text){
+        ArrayList<updates_table> filterlist=new ArrayList<>();
+        for(updates_table users1: UpdatesArrayList){
+            if(users1.getTitle().toLowerCase().contains(text.toLowerCase())){
+                filterlist.add(users1);
+            } else if (users1.getDescription().toLowerCase().contains(text.toLowerCase())) {
+                filterlist.add(users1);
+            }
+        }
+        if(filterlist.isEmpty()){
+            Toast.makeText(getApplicationContext(),"NO Update FOUND",Toast.LENGTH_SHORT).show();
+        }else{
+            adapter.setfilterdupdatelist(filterlist);
+        }
+
     }
 }
